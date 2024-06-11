3967d41186e64ae74204f9ba8b4924f40e22cff2
Buidlboy20 <152554575+Buidlboy20@users.noreply.github.com>
2024-06-11 17:13:50 -0500

    Update Install.py

--- Install.py
+++ Install.py
@@ -4,11 +4,20 @@ print("Updating APT")
 os.system("sudo apt update")
 if sys.prefix != sys.base_prefix:
   ask = input("Use existing venv? Y/N")
-  if ask == "Y"
+  if ask == "Y":
     print("Using existing venv")
   else:
-    ask = input("Please input venv activation path. (E.G. venv/bin/activate)")
-    os.system("source " + ask)
+    ask = input("A: Would you rather input venv activation path. (E.G. venv/bin/activate) B: Or would you rather create one? A/B")
+    if ask == "A":
+      ask = input("venv activation path:")
+      os.system("source " + ask)
+    else:
+      print("Creating new venv with name Jarvenv")
+      print("Installing python3-venv dependancy (Password may be required)")
+      os.system("sudo apt-get install python3-venv")
+      os.system("cd")
+      os.system("python3 -m venv Jarvenv")
+      os.system("source Jarvenv/bin/activate")
 else:
   print("No venv detected.")
   ask = input("A: Would you like to create a venv, or B: specify a venv activation path? (E.G. venv/bin/activate) A/B")
