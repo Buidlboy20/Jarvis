376780d1f819c4c9e24e6ec57657934487598c76
Buildboy20 <152554575+Buidlboy20@users.noreply.github.com>
2024-06-12 14:46:12 -0500

    Adding unofficial Repo Support

--- addons.py
+++ addons.py
@@ -1,17 +1,28 @@
 import os
-def listaddons():
-    return [
-        "TpLink-kasa",
-        "spotify",
-        "Roku"
-    ]
+import json
+with open("AddonRepo.json") as file:
+    addons = json.load(file)
+
+
 def enableaddon(addon):
     if not(addon in os.environ['EnAddons']):
         os.environ['EnAddons'] = os.environ['EnAddons'] + f" '{addon}'"
     else:
         print("Addon already installed.")
         
-def installaddon(addonnum):
-    addon = listaddons[addonnum]
-    if addon == "TpLink-kasa":
-        os.system("pip install python-kasa")
+
+print("Activating Addon Installer")
+os.system("sudo apt install git")
+os.system(os.environ['JARVISVENVPATH'])
+os.system("pip install gitpython")
+import git
+def gitclone(url):
+    return git.Repo.clone_from(url)
+git.Repo.clone_from(
+ask = input("A: Install from official repo. B: Install from other repo. (May not be compatible) A/B ")
+if ask == "A":
+    for i in addons:
+        print(\n + i + ": " + addons[i])
+        print(\n + " Please choose a number:")
+        ask = input("")
+        
