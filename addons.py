4b15563c1863eca829bcdf4b94b3f6c7ccc23f6d
Buidlboy20 <152554575+Buidlboy20@users.noreply.github.com>
2024-06-11 23:05:49 -0500

    AddonInstaller

--- addons.py
+++ addons.py
@@ -3,5 +3,15 @@ def listaddons():
     return [
         "TpLink-kasa",
         "spotify",
-        ""
+        "Roku"
     ]
+def enableaddon(addon):
+    if not(addon in os.environ['EnAddons']):
+        os.environ['EnAddons'] = os.environ['EnAddons'] + f" '{addon}'"
+    else:
+        print("Addon already installed.")
+        
+def installaddon(addonnum):
+    addon = listaddons[addonnum]
+    if addon == "TpLink-kasa":
+        os.system("pip install python-kasa")
