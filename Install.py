2b573752c2b7f021a8160a8a29ea38cadf157a23
Buildboy20 <152554575+Buidlboy20@users.noreply.github.com>
2024-06-13 14:09:21 -0700

    Removed unnecessary lines

--- Install.py
+++ Install.py
@@ -3,40 +3,9 @@ import sys
 os.system("source")
 print("Updating APT")
 os.system("sudo apt update")
-if sys.prefix != sys.base_prefix:
-  ask = input("Use existing venv? Y/N")
-  if ask == "Y":
-    print("Using existing venv")
-  else:
-    ask = input("A: Would you rather input venv activation path. (E.G. venv/bin/activate) B: Or would you rather create one? A/B")
-    if ask == "A":
-      ask = input("venv activation path:")
-      os.system("source " + ask)
-      os.environ['JARVISVENVPATH'] = "~/" + ask
-    else:
-      print("Creating new venv with name Jarvenv")
-      print("Installing python3-venv dependancy (Password may be required)")
-      os.system("sudo apt-get install python3-venv")
-      os.system("cd")
-      os.system("python3 -m venv Jarvenv")
-      os.system("source ~/Jarvis/Jarvenv/bin/activate")
-      os.environ['JARVISVENVPATH'] = '~/Jarvis/Jarvenv/bin/activate'
-else:
-  print("No venv detected.")
-  ask = input("A: Would you like to create a venv, or B: specify a venv activation path? (E.G. venv/bin/activate) A/B")
-  if ask == "A":
-    print("Creating new venv with name Jarvenv")
-    print("Installing python3-venv dependancy (Password may be required)")
-    os.system("sudo apt-get install python3-venv")
-    os.system("cd")
-    os.system("python3 -m venv ~/Jarvis/Jarvenv")
-    os.system("source ~/Jarvis/Jarvenv/bin/activate")
-    os.environ['JARVISVENVPATH'] = '~/Jarvenv/bin/activate'
-#Install as a service.
-  else:
-    ask = input("venv activation path:")
-    os.system("source " + ask)
-    os.environ['JARVISVENVPATH'] = "~/" + ask
+if not sys.prefix != sys.base_prefix:
+  print("Warning! No venv detected.")
+  exit()
 with open("/etc/systemd/system/Jarvis.service", "w") as file:
   file.write("""
 [Unit]
