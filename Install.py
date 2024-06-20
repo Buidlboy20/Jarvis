2e9e9b9472d77532da7b804a6174e99b7705ae5d
Buildboy20 <152554575+Buidlboy20@users.noreply.github.com>
2024-06-19 19:29:56 -0500

    Updated

--- Install.py
+++ Install.py
@@ -4,7 +4,7 @@ os.system("source")
 print("Updating APT")
 os.system("sudo apt update")
 if not sys.prefix != sys.base_prefix:
-  print("Warning! No venv detected.")
+  print("Error! No venv detected.")
   exit()
 
 ask = input("Would you like you use the offline speech to text engine? Y/N ")
@@ -19,5 +19,9 @@ os.system("sudo systemctl enable Jarvis.service ")
 os.system("sudo apt install python3-dev portaudio19-dev python3-pyaudio pip -y")
 os.system("wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_GB/northern_english_male/medium/en_GB-northern_english_male-medium.onnx?download=true")
 os.system("wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_GB/northern_english_male/medium/en_GB-northern_english_male-medium.onnx.json?download=true.json")
+os.system("python3 -m pip install --upgrade pip")
+os.system("python3 -m pip install xmltodict speechRecognition nltk playsound gTTS weather2 wikipedia google transformers torch simpleaudio pygame pydub flask pyalsaaudio pyaudio")
+os.environ['JARVISVENVPATH'] = ""
 os.system("pip install --upgrade pip")
 os.system("pip install speechRecognition nltk playsound gTTS weather2 wikipedia google transformers torch simpleaudio pygame pydub flask pyalsaaudio pyaudio")
+
--- addons.py
+++ addons.py
@@ -1,4 +1,55 @@
 import os
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import json
+import xml
+def listaddons():
+    return [
+        "TpLink-kasa", #1
+        "spotify", #2
+        "Roku" #3
+    ]
+intents = [
+        {
+        "tag": "TpLink-kasa",
+        "patterns": [ "Turn off the light.", "Turn off the light", "Turn on my switch", "Turn off my switch"
+        ]
+        },
+        {
+        "tag": "Volume",
+        "patterns": [
+            "",
+            "",
+
+        ]
+        }
+
+]
+def match_intent(text):
+    tokens = word_tokenize(text)
+    tokens = [word.lower() for word in tokens if word.isalpha()]
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+
+    best_match = None
+    best_score = 0
+
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            pattern_tokens = word_tokenize(pattern)
+            pattern_tokens = [word.lower() for word in pattern_tokens if word.isalpha()]
+            pattern_tokens = [word for word in pattern_tokens if word not in stopwords.words('english')]
+
+            score = len(set(tokens) & set(pattern_tokens))  # Calculate overlap score
+
+            if score > best_score:
+                best_match = intent
+                best_score = score
+
+    if best_match:
+        return best_match
+    
+
 import json
 with open("AddonRepo.json") as file:
     addons = json.load(file)
@@ -9,7 +60,30 @@ def enableaddon(addon):
         os.environ['EnAddons'] = os.environ['EnAddons'] + f" '{addon}'"
     else:
         print("Addon already installed.")
+def intentcheck(output):
+    for i in os.environ['EnAddons']:
+        match_intent(output)
+def installaddon(addonnum):
+    addon = listaddons[addonnum]
+    if addon == "TpLink-kasa":
+        os.system("pip install python-kasa")
+        enableaddon(addon)
+    elif addon == "Roku":
+        os.system("pip install roku")
+        from roku import Roku
+        roku = Roku()
+        for i in roku.discovery():
         
+
+            import requests
+            import xmltodict
+            url = roku.discovery()[i]
+            response = requests.get(url)
+            data = xmltodict.parse(response.content)
+            data['root']['device']['friendlyname']
+
+
+            
 
 print("Activating Addon Installer")
 os.system("sudo apt install git")
@@ -29,3 +103,4 @@ if ask == "A":
         os.system("git clone " + addon2install + " addons/")
         os.system("python3 addons/setup.py")
         
+
